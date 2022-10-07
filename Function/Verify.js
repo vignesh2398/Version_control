26f59de92e996ad5e3d4e793cf68b7ab5f569580
vignesh2398 <rvignesh60474@gmail.com>
2022-10-07 19:25:22 +0530

    verf commit

--- Function/Verify.js
+++ Function/Verify.js
@@ -2,23 +2,24 @@ const jwt = require("jsonwebtoken")
 
 var verifys = async(req, res, next)=>{
     try {
+        console.log("hello")
        
-        var token=req.header("auth-token");
-        var email=req.header("email");
-       var verified = jwt.verify(token, process.env.Token);
+     //   var token=req.header("auth-token");
+    //    var email=req.header("email");
+//var verified = jwt.verify(token, process.env.Token);
        
-        console.log(email==verified.email)
-        console.log(verified)
-        console.log(email)
-        if(email==verified.email)
-        {
+     //   console.log(email==verified.email)
+     //   console.log(verified)
+     //   console.log(email)
+     //   if(email==verified.email)
+//{
 
-            next(); 
-        }
-        else{
-                res.send({success:false,data:"unauthoriseuser"})
-                console.log("unauthoriseuser")
-        }
+//next(); 
+   //     }
+    //    else{
+      //          res.send({success:false,data:"unauthoriseuser"})
+     //           console.log("unauthoriseuser")
+       // }
     } catch (error) {
         console.log("error")
         res.send(error)
